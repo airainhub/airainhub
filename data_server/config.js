8065a41ce0fd8549578a57dcf77afeb49cdc4506
wood <23395306@qq.com>
2017-11-23 12:14:26 +0800

    1
    1

--- data_server/config.js
+++ data_server/config.js
@@ -17,7 +17,7 @@ module.exports = {
         host			:	'127.0.0.1',
         port			:	3306,
         user     		: 	'root',
-        password 		: 	'airainhub2017',
+        password 		: 	'airain2017',
         database		: 	'airain'
     }
 };
